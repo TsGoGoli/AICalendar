9ed239dadf683fb7f163b26d492ab6b671411208
TSOTNE GOGOLI <tsotne.gogoli543@ens.tsu.edu.ge>
2025-06-17 00:36:52 +0400

    Add scheduling service with algorithm for finding available time slots

---
+++ src/Domain/Services/ISchedulingService.cs
@@ -0,0 +1,25 @@
+using AICalendar.Domain.Common;
+using AICalendar.Domain.Entities;
+using AICalendar.Domain.ValueObjects;
+
+namespace AICalendar.Domain.Services;
+
+/// <summary>
+/// Interface for the scheduling service that finds available time slots for users
+/// </summary>
+public interface ISchedulingService
+{
+    /// <summary>
+    /// Finds available time slots for a group of users within a specified time range
+    /// </summary>
+    /// <param name="users">The list of users to find available slots for</param>
+    /// <param name="searchRange">The time range to search within</param>
+    /// <param name="duration">The minimum duration needed for the meeting</param>
+    /// <param name="maxResults">Maximum number of slots to return</param>
+    /// <returns>A list of available time slots sorted by start time</returns>
+    Result<IReadOnlyList<DateTimeRange>> FindAvailableSlots(
+        IEnumerable<User> users,
+        DateTimeRange searchRange,
+        TimeSpan duration,
+        int maxResults = 5);
+}

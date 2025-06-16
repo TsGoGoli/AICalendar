80df4599303833e54c79b5e729ea7957a1e254ba
TSOTNE GOGOLI <tsotne.gogoli543@ens.tsu.edu.ge>
2025-06-17 02:08:54 +0400

    Fix variance issue in IQuery interface to fix build

--- src/Application/Common/Queries/IQuery.cs
+++ src/Application/Common/Queries/IQuery.cs
@@ -4,9 +4,9 @@ using MediatR;
 namespace AICalendar.Application.Common.Queries;
 
 /// <summary>
-/// Base interface for all queries
+/// Base interface for queries
 /// </summary>
 /// <typeparam name="TResponse">The type of the response</typeparam>
-public interface IQuery<out TResponse> : IRequest<Result<TResponse>>
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
 {
 }
